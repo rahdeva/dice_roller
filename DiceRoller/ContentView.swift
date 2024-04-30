1673215a956fab85e457dbfcf678c7ca7a0e0999
Ngurah Deva <wirandanaputra@gmail.com>
2024-04-30 15:36:55 +0700

    finish challenge

--- DiceRoller/ContentView.swift
+++ DiceRoller/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberOfDice: Int = 1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dice Roller")
+                .font(.title.lowercaseSmallCaps())
+                .foregroundStyle(.white)
+            HStack{
+                ForEach(1...numberOfDice, id: \.self){
+                    _ in DiceView()
+                        .foregroundStyle(.white)
+                }
+            }
+            .padding()
+            HStack{
+                Button("Remove", systemImage: "minus.circle.fill"){
+                    numberOfDice -= 1
+                }
+                .disabled(numberOfDice == 1)
+                .labelStyle(.iconOnly)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                Button("Add", systemImage: "plus.circle.fill"){
+                    numberOfDice += 1
+                }
+                .disabled(numberOfDice == 5)
+                .labelStyle(.iconOnly)
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            }
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.BG)
+        .tint(.white)
     }
 }
 
