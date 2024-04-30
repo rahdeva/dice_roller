98113936253d07cd60f8ea63e5a3ef6edf260cd1
Ngurah Deva <wirandanaputra@gmail.com>
2024-04-30 14:43:42 +0700

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by MacBook Air on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
